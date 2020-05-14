18543324bbdebb42722ed81c42d559ac512c82d1
Haxton <realhaxtonsale@yandex.ru>
2020-05-14 15:47:32 +0300

    Init

---
+++ src/extension.cpp
@@ -0,0 +1,182 @@
+#include "extension.h"
+
+/**
+ * @file extension.cpp
+ * @brief Implement extension code here.
+ */
+
+LoadSoundscript g_LoadSoundscript;		/**< Global singleton for extension's main interface */
+SMEXT_LINK(&g_LoadSoundscript);
+
+ISoundEmitterSystemBase *soundemittersystem;
+IBinTools *bintools;
+
+void *g_pAddSnd;
+ICallWrapper *g_pCallAddSoundsFromFile;
+
+sp_nativeinfo_t g_ExtensionNatives[] =
+{
+	{ "LoadSoundScript",			Native_LoadSoundscript },
+	{ NULL,							NULL }
+};
+
+//From SM's stringutil.cpp
+size_t UTIL_DecodeHexString(unsigned char* buffer, size_t maxlength, const char* hexstr)
+{
+	size_t written = 0;
+	size_t length = strlen(hexstr);
+
+	for (size_t i = 0; i < length; i++)
+	{
+		if (written >= maxlength)
+			break;
+		buffer[written++] = hexstr[i];
+		if (hexstr[i] == '\\' && hexstr[i + 1] == 'x')
+		{
+			if (i + 3 >= length)
+				continue;
+			/* Get the hex part. */
+			char s_byte[3];
+			int r_byte;
+			s_byte[0] = hexstr[i + 2];
+			s_byte[1] = hexstr[i + 3];
+			s_byte[2] = '\0';
+			/* Read it as an integer */
+			sscanf(s_byte, "%x", &r_byte);
+			/* Save the value */
+			buffer[written - 1] = r_byte;
+			/* Adjust index */
+			i += 3;
+		}
+	}
+
+	return written;
+}
+
+//By https://github.com/peace-maker
+void* GetAddressFromKeyValues(void* pBaseAddr, IGameConfig* pGameConfig, const char* sKey)
+{
+	const char* value = pGameConfig->GetKeyValue(sKey);
+	if (!value)
+		return nullptr;
+
+	// Got a symbol here.
+	if (value[0] == '@')
+		return memutils->ResolveSymbol(pBaseAddr, &value[1]);
+
+	// Convert hex signature to byte pattern
+	unsigned char signature[200];
+	size_t real_bytes = UTIL_DecodeHexString(signature, sizeof(signature), value);
+	if (real_bytes < 1)
+		return nullptr;
+
+#ifdef _LINUX
+	// The pointer returned by dlopen is not inside the loaded librarys memory region.
+	struct link_map* dlmap = (struct link_map*)pBaseAddr;
+	pBaseAddr = (void*)dlmap->l_addr;
+#endif
+
+	// Find that pattern in the pointed module.
+	return memutils->FindPattern(pBaseAddr, (char*)signature, real_bytes);
+}
+
+void AddSoundsFromFile(const char* filename, bool bPreload, bool bIsOverride, bool bRefresh)
+{
+	ArgBuffer<ISoundEmitterSystemBase*, const char*, bool, bool, bool> vstk(soundemittersystem, filename, bPreload, bIsOverride, bRefresh);
+	g_pCallAddSoundsFromFile->Execute(vstk, nullptr);
+}
+
+bool LoadSoundscript::SDK_OnLoad(char *error, size_t maxlen, bool late)
+{
+	sharesys->AddDependency(myself, "bintools.ext", true, true);
+
+	IGameConfig* pGameConfig;
+	char sError[255];
+	if (!gameconfs->LoadGameConfigFile("loadsoundscript.games", &pGameConfig, sError, sizeof(sError)))
+	{
+		ke::SafeSprintf(error, maxlen, "Could not load loadsoundscript.games gamedata: %s", sError);
+		return false;
+	}
+
+#ifdef WIN32
+	// Find soundemittersystem.dll module in memory
+	HMODULE hSndEmitterSys = GetModuleHandleA("soundemittersystem.dll");
+	if (!hSndEmitterSys)
+	{
+		ke::SafeStrcpy(error, maxlen, "Could not find the soundemittersystem library in memory.");
+		return false;
+	}
+#else // WIN32
+	void* hSndEmitterSys = dlopen("soundemittersystem_srv.so", RTLD_LAZY);
+	if (!hSndEmitterSys)
+		hSndEmitterSys = dlopen("soundemittersystem.so", RTLD_LAZY);
+
+	if (!hSndEmitterSys)
+	{
+		ke::SafeStrcpy(error, maxlen, "Could not find the soundemittersystem library in memory.");
+		return false;
+	}
+#endif
+
+	g_pAddSnd = GetAddressFromKeyValues(hSndEmitterSys, pGameConfig, "CSoundEmitterSystemBase::AddSoundsFromFile");
+	if (!g_pAddSnd)
+	{
+		ke::SafeStrcpy(error, maxlen, "Could not find the CSoundEmitterSystemBase::AddSoundsFromFile signature");
+		return false;
+	}
+
+	PassInfo passinfo[] = {
+		{PassType_Basic, PASSFLAG_BYVAL, sizeof(char*), NULL, NULL},
+		{PassType_Basic, PASSFLAG_BYVAL, sizeof(bool), NULL, NULL},
+		{PassType_Basic, PASSFLAG_BYVAL, sizeof(bool), NULL, NULL},
+		{PassType_Basic, PASSFLAG_BYVAL, sizeof(bool), NULL, NULL}
+	};
+
+	g_pCallAddSoundsFromFile = bintools->CreateCall(g_pAddSnd, CallConv_ThisCall, NULL, passinfo, 4);
+	if (!g_pCallAddSoundsFromFile)
+	{
+		ke::SafeStrcpy(error, maxlen, "Could not create call to CSoundEmitterSystemBase::AddSoundsFromFile");
+		return false;
+	}
+
+	gameconfs->CloseGameConfigFile(pGameConfig);
+
+	return true;
+}
+
+void LoadSoundscript::SDK_OnAllLoaded()
+{
+	SM_GET_LATE_IFACE(BINTOOLS, bintools);
+
+	sharesys->AddNatives(myself, g_ExtensionNatives);
+	sharesys->RegisterLibrary(myself, "LoadSoundscript");
+}
+
+bool LoadSoundscript::QueryRunning(char* error, size_t maxlength)
+{
+	SM_CHECK_IFACE(BINTOOLS, bintools);
+
+	return true;
+}
+
+void LoadSoundscript::SDK_OnUnload()
+{
+	g_pCallAddSoundsFromFile->Destroy();
+}
+
+bool LoadSoundscript::SDK_OnMetamodLoad(ISmmAPI* ismm, char* error, size_t maxlen, bool late)
+{
+	GET_V_IFACE_CURRENT(GetFileSystemFactory, soundemittersystem, ISoundEmitterSystemBase, SOUNDEMITTERSYSTEM_INTERFACE_VERSION);
+
+	return true;
+}
+
+static cell_t Native_LoadSoundscript(IPluginContext *pContext, const cell_t *params)
+{
+	char *sPath;
+	pContext->LocalToString(params[1], &sPath);
+
+	AddSoundsFromFile(sPath, params[2], params[3], params[4]);
+
+	return NULL;
+}
---
+++ src/extension.h
@@ -0,0 +1,96 @@
+#ifndef _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
+#define _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
+
+/**
+ * @file extension.h
+ * @brief Sample extension code header.
+ */
+
+#include "sdk/smsdk_ext.h"
+#include "extensions/iBinTools.h"
+#include <sm_argbuffer.h>
+#include "SoundEmitterSystem/isoundemittersystembase.h"
+
+/**
+ * @brief Sample implementation of the SDK Extension.
+ * Note: Uncomment one of the pre-defined virtual functions in order to use it.
+ */
+class LoadSoundscript : public SDKExtension
+{
+public:
+	/**
+	 * @brief This is called after the initial loading sequence has been processed.
+	 *
+	 * @param error		Error message buffer.
+	 * @param maxlen	Size of error message buffer.
+	 * @param late		Whether or not the module was loaded after map load.
+	 * @return			True to succeed loading, false to fail.
+	 */
+	virtual bool SDK_OnLoad(char *error, size_t maxlen, bool late);
+	
+	/**
+	 * @brief This is called right before the extension is unloaded.
+	 */
+	virtual void SDK_OnUnload();
+
+	/**
+	 * @brief This is called once all known extensions have been loaded.
+	 * Note: It is is a good idea to add natives here, if any are provided.
+	 */
+	virtual void SDK_OnAllLoaded();
+
+	/**
+	 * @brief Called when the pause state is changed.
+	 */
+	//virtual void SDK_OnPauseChange(bool paused);
+
+	/**
+	 * @brief this is called when Core wants to know if your extension is working.
+	 *
+	 * @param error		Error message buffer.
+	 * @param maxlen	Size of error message buffer.
+	 * @return			True if working, false otherwise.
+	 */
+	virtual bool QueryRunning(char *error, size_t maxlength);
+public:
+#if defined SMEXT_CONF_METAMOD
+	/**
+	 * @brief Called when Metamod is attached, before the extension version is called.
+	 *
+	 * @param error			Error buffer.
+	 * @param maxlen		Maximum size of error buffer.
+	 * @param late			Whether or not Metamod considers this a late load.
+	 * @return				True to succeed, false to fail.
+	 */
+	virtual bool SDK_OnMetamodLoad(ISmmAPI *ismm, char *error, size_t maxlen, bool late);
+
+	/**
+	 * @brief Called when Metamod is detaching, after the extension version is called.
+	 * NOTE: By default this is blocked unless sent from SourceMod.
+	 *
+	 * @param error			Error buffer.
+	 * @param maxlen		Maximum size of error buffer.
+	 * @return				True to succeed, false to fail.
+	 */
+	//virtual bool SDK_OnMetamodUnload(char *error, size_t maxlen);
+
+	/**
+	 * @brief Called when Metamod's pause state is changing.
+	 * NOTE: By default this is blocked unless sent from SourceMod.
+	 *
+	 * @param paused		Pause state being set.
+	 * @param error			Error buffer.
+	 * @param maxlen		Maximum size of error buffer.
+	 * @return				True to succeed, false to fail.
+	 */
+	//virtual bool SDK_OnMetamodPauseChange(bool paused, char *error, size_t maxlen);
+#endif
+};
+
+size_t UTIL_DecodeHexString(unsigned char *buffer, size_t maxlength, const char *hexstr);
+void* GetAddressFromKeyValues(void *pBaseAddr, IGameConfig *pGameConfig, const char *sKey);
+void AddSoundsFromFile(const char* filename, bool bPreload, bool bIsOverride, bool bRefresh);
+
+static cell_t Native_LoadSoundscript(IPluginContext *pContext, const cell_t *params);
+
+#endif // _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
